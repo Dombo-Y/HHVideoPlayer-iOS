820b8a7346b8fddeffae2bac8bfcd65bab568fcc
Dombo-Y <364533540@qq.com>
2023-04-13 00:36:33 +0800

    改库改了两天

--- HHVideoPlayer-iOS/HHVideoPlayer-iOS/HHHeader.h
+++ HHVideoPlayer-iOS/HHVideoPlayer-iOS/HHHeader.h
@@ -32,19 +32,7 @@ extern "C" {
 #include "libswresample/swresample.h"
 
 
-#include "SDL.h"
-#include "SDL_audio.h"
-#include "SDL_endian.h"
-#include "SDL_error.h"
-#include "SDL_events.h"
-#include "SDL_loadso.h"
-#include "SDL_main.h"
-#include "SDL_mutex.h"
-#include "SDL_rwops.h"
-#include "SDL_stdinc.h"
-#include "SDL_thread.h"
-#include "SDL_timer.h"
-#include "SDL_video.h"
+#include "SDL.h" 
 
 #ifdef __cplusplus
 };
